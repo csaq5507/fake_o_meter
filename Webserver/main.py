80028861f52606b7378572f57a8be02dee38feca
Ivan Waldboth <ivan@waldboth.com>
2020-04-26 23:49:20 +0200

    Working Prototype

--- Webserver/main.py
+++ Webserver/main.py
@@ -1,18 +1,87 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import detect
-import time
 from io import BytesIO
-from urllib.parse import urlparse, parse_qs
+import cgi
+import os
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras import backend as K
+import pickle
 
+##
+#
+# You first need to train the model so a tokenizer.pickle(dictionary) & the trained weights get generated
+#
+##
+
+# initialize neural network
+K.clear_session()
+
+# Running on a apache2 server, reverse proxy to this directory
 hostName = "localhost"
 serverPort = 8080
 
+# Create a new model instance
+with open('tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
+
+vocab_size = len(tokenizer.word_index)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size + 1, 100),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Conv1D(64, 5, activation='relu'),
+    tf.keras.layers.MaxPooling1D(pool_size=4),
+    tf.keras.layers.LSTM(20, return_sequences=True),
+    tf.keras.layers.LSTM(20),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(512),
+    tf.keras.layers.Dropout(0.3),
+    tf.keras.layers.Dense(256),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+checkpoint_path = "training_weights/cp.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+latest = tf.train.latest_checkpoint(checkpoint_dir)
+model.load_weights(latest)
+
+
+# Neural network initialized
+
+# NN detector
+
+def detect(data):
+    sequences = tokenizer.texts_to_sequences([data])
+    padded = pad_sequences(sequences, maxlen=500, padding='post', truncating='post')
+    data = np.array(padded)
+    result = model.predict(data)
+    return result[0][0]
+
+
+# Webserver
 class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Why u do this?')
+
     def do_POST(self):
-        length = int(self.headers.getheader('content-length'))
-        field_data = self.rfile.read(length)
-        fields = urlparse.parse_qs(field_data)
-        if(fields.text is None):
+        form = cgi.FieldStorage(
+            fp=self.rfile,
+            headers=self.headers,
+            environ={'REQUEST_METHOD': 'POST',
+                     'CONTENT_TYPE': self.headers['Content-Type'],
+                     })
+        text = 0
+
+        for field in form.keys():
+            if field == "text":
+                text = form[field]
+
+        if text == 0:
             self.send_response(200)
             self.end_headers()
             response = BytesIO()
@@ -21,11 +90,11 @@ class MyServer(BaseHTTPRequestHandler):
         else:
             self.send_response(200)
             self.end_headers()
-            response = BytesIO()
-            response.write(detect.detect(fields.text))
-            self.wfile.write(response.getvalue())
+            result = detect(text.value)
+            self.wfile.write(str(result).encode("utf8"))
 
-if __name__ == "__main__":        
+
+if __name__ == "__main__":
     webServer = HTTPServer((hostName, serverPort), MyServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
 
--- Webserver/train.py
+++ Webserver/train.py
@@ -3,48 +3,48 @@ import os
 import tensorflow as tf
 import numpy as np
 import pandas as pd
-import io
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.utils import to_categorical
-from tensorflow.keras import regularizers
-
+import pickle
 
 from tensorflow.keras import backend as K
+
 K.clear_session()
 
 # Check tensorflow version
 if float(tf.__version__[0]) < 2.0:
-  print('Need to update tensorflow')
-  exit()
+    print('Need to update tensorflow')
+    exit()
 else:
-  print('Correct version of Tensorflow installed.')
+    print('Correct version of Tensorflow installed.')
 
+# read training data
 df = pd.read_csv('training_data/train.csv')
 df = df.fillna(' ')
 df.count()
 
 tokenizer = Tokenizer()
 tokenizer.fit_on_texts(df['text'])
+with open('tokenizer.pickle', 'wb') as handle:
+    pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
 word_index = tokenizer.word_index
-vocab_size=len(word_index)
+vocab_size = len(word_index)
 print(vocab_size)
 
 # Padding data
-
 sequences = tokenizer.texts_to_sequences(df['text'])
 padded = pad_sequences(sequences, maxlen=500, padding='post', truncating='post')
 
-
 split = 0.2
-split_n = int(round(len(padded)*(1-split),0))
+split_n = int(round(len(padded) * (1 - split), 0))
 
 train_data = padded[:split_n]
 train_labels = df['label'].values[:split_n]
 test_data = padded[split_n:]
 test_labels = df['label'].values[split_n:]
 
-
+# Global Vectors for Word Representation
 embeddings_index = {}
 with open('tmp/glove.6B.100d.txt') as f:
     for line in f:
@@ -54,42 +54,39 @@ with open('tmp/glove.6B.100d.txt') as f:
         embeddings_index[word] = coefs
 print(len(coefs))
 
-embeddings_matrix = np.zeros((vocab_size+1, 100))
+embeddings_matrix = np.zeros((vocab_size + 1, 100))
 for word, i in word_index.items():
     embedding_vector = embeddings_index.get(word)
     if embedding_vector is not None:
         embeddings_matrix[i] = embedding_vector
 
+# create Model
 model = tf.keras.Sequential([
-  tf.keras.layers.Embedding(vocab_size + 1, 100, weights=[embeddings_matrix], trainable=False),
-  tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Conv1D(64, 5, activation='relu'),
-  tf.keras.layers.MaxPooling1D(pool_size=4),
-  tf.keras.layers.LSTM(20, return_sequences=True),
-  tf.keras.layers.LSTM(20),
-  tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(512),
-  tf.keras.layers.Dropout(0.3),
-  tf.keras.layers.Dense(256),
-  tf.keras.layers.Dense(1, activation='sigmoid')
+    tf.keras.layers.Embedding(vocab_size + 1, 100, weights=[embeddings_matrix], trainable=False),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Conv1D(64, 5, activation='relu'),
+    tf.keras.layers.MaxPooling1D(pool_size=4),
+    tf.keras.layers.LSTM(20, return_sequences=True),
+    tf.keras.layers.LSTM(20),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(512),
+    tf.keras.layers.Dropout(0.3),
+    tf.keras.layers.Dense(256),
+    tf.keras.layers.Dense(1, activation='sigmoid')
 ])
 
+# Create a callback that saves the model's weights
 checkpoint_path = "training_weights/cp.ckpt"
 checkpoint_dir = os.path.dirname(checkpoint_path)
 
-# Create a callback that saves the model's weights
 cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
                                                  save_weights_only=True,
                                                  verbose=1)
 
-model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.summary()
 
-history = model.fit(train_data, train_labels, epochs=5, batch_size=100, validation_data=[test_data, test_labels],
-          callbacks=[cp_callback])
-
+# https://youtu.be/mw2kKyJu9gY?t=130
+history = model.fit(train_data, train_labels, epochs=5, batch_size=100, validation_data=[test_data, test_labels], callbacks=[cp_callback])
 
 print("Training Complete")
-
-
-
